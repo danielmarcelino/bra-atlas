711ae5341748f41646475875fad1df78da787725
daniel <dmarcelino@live.com>
2020-11-11 11:52:47 -0300

    update

---
+++ social/localidades.py
@@ -0,0 +1,11 @@
+import requests
+import json 
+
+headers = {
+    'Content-Type': 'application/json;charset=UTF-8',
+    'User-Agent': 'social.py - https://github.com/danielmarcelino/jota_eleitoral',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Connection': 'keep-alive',
+}
