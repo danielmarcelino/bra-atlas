d44f0aef72198dc7d2d0088d77db1d0e466be114
daniel <dmarcelino@live.com>
2020-11-11 11:50:05 -0300

    update

--- ibge/localidades.py
+++ ibge/localidades.py
@@ -9,7 +9,7 @@ import json
 
 headers = {
     'Content-Type': 'application/json;charset=UTF-8',
-    'User-Agent': 'ibge.py - https://github.com/leogregianin/ibge',
+    'User-Agent': 'ibge.py - https://github.com/danielmarcelino/jota_eleitoral',
     'Accept': 'application/json, text/plain, */*',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
