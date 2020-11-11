d85f97b4129daba0b41399c7e33baca744b6b312
daniel <dmarcelino@live.com>
2020-11-11 11:11:20 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Daniel Marcelino",
+    author_email="dmarcelino@live.com",
+    name='ibge',
+    license="MIT",
+    description='Collection of APIs for the IBGE Data Services in Brazil',
+    version='0.0.5',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/danielmarcelino/ibge_eleitoral/',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Portuguese (Brazilian)'
+    ],
+    project_urls={
+        'Documentation': 'https://github.com/danielmarcelino/ibge_eleitoral/blob/master/README.md'
+    }
+)
